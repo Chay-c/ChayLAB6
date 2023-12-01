fb53138123d1f650a44bb1c4061d87dfa711fdf2
Chay <chayangkul_c@cmu.ac.th>
2023-12-01 15:45:47 +0700

    lab6_2.cpp completed

--- lab6_2.cpp
+++ lab6_2.cpp
@@ -20,7 +20,7 @@ double pythagoras(double xcomp,double ycomp){
 void showResult(double result_vec_length, double result_vec_direction){
     cout << "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"<< endl;
     cout << "Length of the resultant vector = " <<result_vec_length<< endl;
-    cout << "Direction of the second vertor (deg) = "<<result_vec_direction<< endl ;
+    cout << "Direction of the resultant vector (deg) = "<<result_vec_direction<< endl ;
     cout << "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%";   
 }
 int main(){
